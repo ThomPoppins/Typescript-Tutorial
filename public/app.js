86d7228f5374d413a43d5175ef250f1f05bee6f9
Thom Poppins <thompoppins@gmail.com>
2023-09-23 13:18:11 +0100

    Interfaces with classes

--- public/app.js
+++ public/app.js
@@ -1,33 +1,22 @@
-const me = {
-    name: "Thom",
-    age: 32,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log("I spent", amount);
-        return amount;
-    },
-    // skills: ["JS", "TS"], // Error because it's not in the interface
-};
-// Enforce the interface on a variable
-let someone;
-// Enforce the interface on a function parameter
-const greetPerson = (person) => {
-    console.log("hello", person.name);
-};
-greetPerson(me);
-console.log(me);
 import { Invoice } from "./classes/Invoice.js";
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-let invoices = [];
-// invoices.push("hello"); // Error
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { Payment } from "./classes/Payment.js";
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice("yoshi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing work", 200);
+// // Array with objects of classes which implement HasFormatter interface
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// const invOne = new Invoice("mario", "work on the mario website", 250);
+// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+// let invoices: Invoice[] = [];
+// // invoices.push("hello"); // Error
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
@@ -36,5 +25,13 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    // doc has to be of class that implements the HasFormatter interface
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,29 @@
-// Interfaces
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
-
-const me: IsPerson = {
-  name: "Thom",
-  age: 32,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    console.log("I spent", amount);
-    return amount;
-  },
-  // skills: ["JS", "TS"], // Error because it's not in the interface
-};
-
-// Enforce the interface on a variable
-let someone: IsPerson;
-
-// Enforce the interface on a function parameter
-const greetPerson = (person: IsPerson) => {
-  console.log("hello", person.name);
-};
-
-greetPerson(me);
-
-console.log(me);
-
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-let invoices: Invoice[] = [];
-// invoices.push("hello"); // Error
-invoices.push(invOne);
-invoices.push(invTwo);
+// docOne = new Invoice("yoshi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing work", 200);
 
-invoices.forEach((inv) => {
-  console.log(inv.client, inv.amount, inv.format());
-});
+// // Array with objects of classes which implement HasFormatter interface
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+
+// const invOne = new Invoice("mario", "work on the mario website", 250);
+// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+// let invoices: Invoice[] = [];
+// // invoices.push("hello"); // Error
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -56,5 +36,14 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  // doc has to be of class that implements the HasFormatter interface
+  let doc: HasFormatter;
+
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  console.log(doc);
 });
