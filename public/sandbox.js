9a7e8dc2cb00546fd4a252f3a7db96770e3cbc1a
Thom Poppins <thompoppins@gmail.com>
2023-09-23 11:10:45 +0100

    Functions and types

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("mario");
+var greet;
+// greet = "hello"; // Error: Type 'string' is not assignable to type '() => void'
+greet = function () { return console.log("Hello, again!"); };
+greet();
+// If a function doesn't return anything, it's return type is void
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 20);
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 7);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,21 @@
-console.log("mario");
+let greet: Function;
+
+// greet = "hello"; // Error: Type 'string' is not assignable to type '() => void'
+
+greet = () => console.log("Hello, again!");
+greet();
+
+// If a function doesn't return anything, it's return type is void
+const add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 10, 20);
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(10, 7);
+console.log(result);
